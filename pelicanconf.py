85e4558abab7cb265a58e9cfffb6db6a98824e4b
Dmitrijs Milajevs <dimazest@gmail.com>
2014-09-13 12:01:42 +0100

    Initial configuration.

---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'The chairs of IWCS 2015'
+SITENAME = u'IWCS 2015'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (
+    ('IWCS 2013', 'http://www.ling.uni-potsdam.de/iwcs2013//'),
+    # ('Python.org', 'http://python.org/'),
+    # ('Jinja2', 'http://jinja.pocoo.org/'),
+    # ('You can modify those links in your config file', '#'),
+)
+
+# Social widget
+SOCIAL = (
+    # ('You can add links in your config file', '#'),
+    # ('Another social link', '#'),
+)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
