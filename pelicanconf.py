66db34c5090801ee00cb1e4a9eb627d880d029ef
Matthew Purver <matt@chatterbox.co>
2015-03-11 12:03:18 +0000

    added schedule page

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,6 +31,7 @@ DISPLAY_PAGES_ON_MENU = False
 MENUITEMS = (
 #    ('Call for papers', 'call-for-papers.html'),
     ('Accepted Papers', 'accepted-papers.html'),
+#    ('Programme', 'programme.html'),
     ('Workshops', 'workshops.html'),
     ('Invited Speakers', 'speakers.html'),
     ('Hackathon', 'hackathon.html'),
